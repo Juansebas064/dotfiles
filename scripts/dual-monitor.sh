b97983a7acbb1c778f8ce14e06a322244d103a7e
Juan S. Ruiz <juansebas064@gmail.com>
2022-09-29 23:27:16 -0500

    Modify script for dual monitor

--- scripts/dual-monitor.sh
+++ scripts/dual-monitor.sh
@@ -3,8 +3,11 @@ source=$(xrandr |grep ' connected' |grep 'HDMI' |awk '{print $1}')
 if [ $source = 'HDMI-1' ]
 then   
     xrandr --output eDP-1 --mode 1368x768 --pos 1368x0 --output HDMI-1 --auto --above eDP-1
+    tint2 -c ~/.config/tint2/tint2rc2 &
     nitrogen --restore
 else
     xrandr -s 1368x768 
+    kilall tint2
+    tint2 &
     nitrogen --restore
 fi
